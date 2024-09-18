78126235b807a06be7e06c19dba1659b8f52a8cb
james <jamesliwood@gmail.com>
2024-09-18 03:25:36 -0400

    added data for each region

---
+++ frontend/src/region.js
@@ -0,0 +1,54 @@
+const regions = {
+  Kanto: {
+    name: "kanto",
+    lower: 1,
+    higher: 151,
+  },
+  Jhoto: {
+    name: "jhoto",
+    lower: 152,
+    higher: 251,
+  },
+  Hoenn: {
+    name: "hoenn",
+    lower: 252,
+    higher: 386,
+  },
+  Sinnoh: {
+    name: "sinnoh",
+    lower: 387,
+    higher: 493,
+  },
+  Unova: {
+    name: "unova",
+    lower: 494,
+    higher: 649,
+  },
+  Kalos: {
+    name: "kalos",
+    lower: 650,
+    higher: 721,
+  },
+  Alola: {
+    name: "alola",
+    lower: 722,
+    higher: 809,
+  },
+  Galar: {
+    name: "galar",
+    lower: 810,
+    higher: 898,
+  },
+  Hisui: {
+    name: "hisui",
+    lower: 899,
+    higher: 905,
+  },
+  Paldea: {
+    name: "paldea",
+    lower: 906,
+    higher: 1025,
+  },
+};
+
+export default regions;
