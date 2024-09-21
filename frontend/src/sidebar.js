ed958985c8b05d4a5d906695dd1e22c77e9c7e00
james <jamesliwood@gmail.com>
2024-09-21 00:57:51 -0400

    added sidebar component

---
+++ frontend/src/sidebar.js
@@ -0,0 +1,45 @@
+import { boardContext } from "./App";
+import { useContext } from "react";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  const { pokedex, setPokedex, setOpenForm } = useContext(boardContext);
+
+  const view = () => {
+    console.log(pokedex);
+  };
+
+  const reset = () => {
+    setPokedex({
+      regions: [],
+    });
+    setOpenForm(true);
+  };
+
+  const handleChange = (e) => {
+    if (!pokedex[e.target.value] || e.target.value === "regions") return;
+
+    // return if name is already locked in.
+
+    fetch([pokedex[e.target.value].url])
+      .then((res) => res.json())
+      .then((data) => {
+        const pokemonID = data.id;
+        document.getElementById(
+          `pokeID-${pokemonID}`
+        ).style.backgroundImage = `url(${data.sprites.front_default})`;
+      });
+
+    e.target.value = "";
+  };
+
+  return (
+    <div className="sidebar">
+      <button onClick={reset}>Reset</button>
+      <button onClick={view}>View Pokedex</button>
+      <input type="text" placeholder="Search" onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Sidebar;
