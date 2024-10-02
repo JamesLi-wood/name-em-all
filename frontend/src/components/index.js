c24085bcb0770d174149938ae65363688a083ce7
james <jamesliwood@gmail.com>
2024-10-02 04:50:09 -0400

    update path

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Form } from "./form";
+export { default as Form } from "./forms/form";
 export { default as Sidebar } from "./sidebar";
 export { default as Board } from "./board";
