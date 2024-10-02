328f90e9514c4151f9e3d9396d82daa0f354ceb6
james <jamesliwood@gmail.com>
2024-10-02 04:45:57 -0400

    added multiple options for the forms

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -1,99 +1,30 @@
-import { useState, useContext } from "react";
-import { boardContext } from "../App";
-import regions from "../utils/region";
+import { useState } from "react";
+import Regions from "./forms/regions";
+import Types from "./forms/types";
 import "./form.css";
 
+const formComponents = {
+  regions: (props) => <Regions {...props} />,
+  types: (props) => <Types {...props} />,
+};
+
 const Form = () => {
-  const [region, setRegion] = useState({
-    Kanto: false,
-    Jhoto: false,
-    Hoenn: false,
-    Sinnoh: false,
-    Unova: false,
-    Kalos: false,
-    Alola: false,
-    Galar: false,
-    Hisui: false,
-    Paldea: false,
-  });
-  const { setPokedex, setOpenForm } = useContext(boardContext);
-  const allRegions = Object.keys(region);
-
-  const handleAction = (e) => {
-    e.preventDefault();
-    const checked = Object.keys(region).filter((key) => region[key] === true);
-    setPokedex((prevState) => ({
-      ...prevState,
-      ["regions"]: checked,
-    }));
-    let pkmnCount = 0;
-
-    checked.forEach(async (key) => {
-      const regionData = regions[key];
-      const limit = regionData.amount;
-      pkmnCount += limit;
-
-      const pokemonData = {};
-      const fetchPromises = [];
-      for (let i = 0; i < limit; i++) {
-        fetchPromises.push(
-          fetch(`https://pokeapi.co/api/v2/pokemon/${i + regionData.lower}`)
-            .then((res) => res.json())
-            .then((data) => {
-              const name = data.species.name;
-              pokemonData[name] = {
-                name: name,
-                id: i + regionData.lower,
-                sprite: data.sprites.front_default,
-                found: false,
-              };
-            })
-        );
-      }
-
-      await Promise.all(fetchPromises);
-
-      setPokedex((prevState) => ({
-        ...prevState,
-        pokemonData: {
-          ...prevState.pokemonData,
-          ...pokemonData,
-        },
-        pkmnCount: pkmnCount,
-      }));
-    });
-
-    setOpenForm(false);
-  };
-
-  const activateChange = (region) => {
-    document.getElementById(region).click();
-  };
-
-  const handleChange = (e) => {
-    const { id, checked } = e.target;
-    setRegion((prevState) => ({
-      ...prevState,
-      [id]: checked,
-    }));
-  };
+  const [role, setRole] = useState("");
+  const RenderFormComponent = formComponents[role];
 
   return (
     <div className="form-wrapper">
-      <form onSubmit={handleAction}>
-        <div>Select Region</div>
-        <div className="region-selection">
-          {allRegions.map((data) => {
-            return (
-              <div key={data} onClick={() => activateChange(data)}>
-                <input type="checkbox" id={data} onChange={handleChange} />
-                <label htmlFor={data}>{data}</label>
-              </div>
-            );
-          })}
-        </div>
-        <input type="submit" />
-      </form>
+      {role ? (
+        <RenderFormComponent setRole={setRole} />
+      ) : (
+        <form>
+          <div className="form-title">Select Mode</div>
+          <div className="region-selection">
+            <div onClick={() => setRole("regions")}>Regions</div>
+            <div onClick={() => setRole("types")}>Types</div>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
