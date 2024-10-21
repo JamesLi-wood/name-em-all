64e53bdbc3e7a8b98eba9a0381be70fdab368775
james <jamesliwood@gmail.com>
2024-10-21 00:39:52 -0400

    created formOptions util

---
+++ frontend/src/utils/formOptions.js
@@ -0,0 +1,36 @@
+const formOptions = {
+  Regions: [
+    "Kanto",
+    "Jhoto",
+    "Hoenn",
+    "Sinnoh",
+    "Unova",
+    "Kalos",
+    "Alola",
+    "Galar",
+    "Hisui",
+    "Paldea",
+  ],
+  Types: [
+    "bug",
+    "dark",
+    "dragon",
+    "electric",
+    "fairy",
+    "fighting",
+    "fire",
+    "flying",
+    "ghost",
+    "grass",
+    "ground",
+    "ice",
+    "normal",
+    "poison",
+    "psychic",
+    "rock",
+    "steel",
+    "water",
+  ],
+};
+
+export default formOptions;
