8ab7b42d732da03bf4121b1f4909c180d374f779
james <jamesliwood@gmail.com>
2024-10-21 00:31:30 -0400

    added component to display options for its respective mode

--- frontend/src/components/forms/form.js
+++ frontend/src/components/forms/form.js
@@ -1,18 +1,13 @@
 import { useState, useContext } from "react";
 import { boardContext } from "../../App";
-import Regions from "./regions";
-import Types from "./types";
 import "./form.css";
-
-const formComponents = {
-  regions: (props) => <Regions {...props} />,
-  types: (props) => <Types {...props} />,
-};
+import FormMode from "./formMode";
+import pokeRegions from "../../utils/pokeRegions";
+import formOptions from "../../utils/formOptions";
 
 const Form = () => {
-  const [role, setRole] = useState("");
+  const [view, setView] = useState("Main");
   const { setPokedex, setOpenForm } = useContext(boardContext);
-  const RenderFormComponent = formComponents[role];
 
   const guess = async () => {
     const pokemonPromises = [];
@@ -45,19 +40,129 @@ const Form = () => {
     }));
   };
 
+  const back = () => {
+    setView("Main");
+  };
+
+  const handleRegionSubmit = async (e, regions) => {
+    e.preventDefault();
+
+    const selectedOptions = Object.keys(regions).filter(
+      (key) => regions[key] === true
+    );
+    const pokemonData = {};
+    let pkmnCount = 0;
+
+    const regionPromises = selectedOptions.map(async (region) => {
+      const regionData = pokeRegions[region];
+      const limit = regionData.amount;
+      pkmnCount += limit;
+
+      const pkmnPromises = [];
+      for (let i = 0; i < limit; i++) {
+        pkmnPromises.push(
+          fetch(
+            `https://pokeapi.co/api/v2/pokemon/${i + regionData.lower}`
+          ).then((res) => res.json())
+        );
+      }
+
+      const pokemonDetails = await Promise.all(pkmnPromises);
+      pokemonDetails.forEach((data) => {
+        const name = data.species.name;
+        pokemonData[name] = {
+          name: name,
+          id: data.id,
+          sprite: data.sprites.front_default,
+          found: false,
+        };
+      });
+    });
+
+    await Promise.all(regionPromises);
+
+    setPokedex((prevState) => ({
+      ...prevState,
+      mode: "regions",
+      regions: selectedOptions,
+      pokemonData: pokemonData,
+      pkmnCount: pkmnCount,
+    }));
+    setOpenForm(false);
+  };
+
+  const handleTypeSubmit = async (e, types) => {
+    e.preventDefault();
+
+    const selectedOptions = Object.keys(types).filter(
+      (key) => types[key] === true
+    );
+    const pokemonData = {};
+    let pokeCount = 0;
+
+    const typePromises = selectedOptions.map(async (type) => {
+      const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+      const pkmnData = await response.json();
+      const pkmnList = pkmnData.pokemon;
+
+      const pkmnPromises = pkmnList.map(async (pkmn) => {
+        return await fetch(pkmn.pokemon.url).then((res) => res.json());
+      });
+
+      const pokemonDetails = await Promise.all(pkmnPromises);
+      pokemonDetails.forEach((data) => {
+        const name = data.species.name;
+
+        if (!pokemonData[name]) {
+          pokemonData[name] = {
+            name: name,
+            id: data.id,
+            sprite: data.sprites.front_default,
+            found: false,
+            type: type,
+          };
+          pokeCount++;
+        }
+      });
+    });
+
+    await Promise.all(typePromises);
+
+    setPokedex((prevState) => ({
+      ...prevState,
+      mode: "types",
+      types: selectedOptions,
+      pokemonData: pokemonData,
+      pkmnCount: pokeCount,
+    }));
+    setOpenForm(false);
+  };
+
+  const submitHandlers = {
+    Regions: handleRegionSubmit,
+    Types: handleTypeSubmit,
+  };
+
+  const modes = Object.keys(formOptions);
   return (
     <div className="form-wrapper">
-      {role ? (
-        <RenderFormComponent setRole={setRole} />
-      ) : (
+      {view === "Main" ? (
         <form>
           <div className="form-title">Select Mode</div>
-          <div className="region-selection">
-            <div onClick={() => setRole("regions")}>Regions</div>
-            <div onClick={() => setRole("types")}>Types</div>
+          <div className="option-selection">
+            {modes.map((mode) => {
+              return <div onClick={() => setView(mode)}>{mode}</div>;
+            })}
             <div onClick={guess}>Guess</div>
           </div>
         </form>
+      ) : (
+        <FormMode
+          mode={view}
+          options={formOptions[view]}
+          back={back}
+          handleSubmit={submitHandlers[view]}
+        />
       )}
     </div>
   );
