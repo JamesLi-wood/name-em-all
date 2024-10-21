0f33f3afb581f2d1b1816223b83cf1a2b19f7f95
james <jamesliwood@gmail.com>
2024-10-21 00:40:12 -0400

    created pokeRegions util

---
+++ frontend/src/utils/pokeRegions.js
@@ -0,0 +1,64 @@
+const pokeRegions = {
+  Kanto: {
+    name: "kanto",
+    lower: 1,
+    higher: 151,
+    amount: 151,
+  },
+  Jhoto: {
+    name: "jhoto",
+    lower: 152,
+    higher: 251,
+    amount: 100,
+  },
+  Hoenn: {
+    name: "hoenn",
+    lower: 252,
+    higher: 386,
+    amount: 135,
+  },
+  Sinnoh: {
+    name: "sinnoh",
+    lower: 387,
+    higher: 493,
+    amount: 107,
+  },
+  Unova: {
+    name: "unova",
+    lower: 494,
+    higher: 649,
+    amount: 156,
+  },
+  Kalos: {
+    name: "kalos",
+    lower: 650,
+    higher: 721,
+    amount: 72,
+  },
+  Alola: {
+    name: "alola",
+    lower: 722,
+    higher: 809,
+    amount: 88,
+  },
+  Galar: {
+    name: "galar",
+    lower: 810,
+    higher: 898,
+    amount: 89,
+  },
+  Hisui: {
+    name: "hisui",
+    lower: 899,
+    higher: 905,
+    amount: 7,
+  },
+  Paldea: {
+    name: "paldea",
+    lower: 906,
+    higher: 1025,
+    amount: 120,
+  },
+};
+
+export default pokeRegions;
